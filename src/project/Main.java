0ae99766594f82f4f28c3bb26da8c9c0ed194958
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-04-25 13:38:45 -0500

    Finished the graph and made version 1 of Prim's Algorithm

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,6 +2,28 @@ package project;
 
 public class Main {
     public static void main(String[] args) {
+        WeightedGraph<String> graph = new WeightedGraphQ<>();
+        setupVideoGraph(graph);
 
+        System.out.println();
+
+    }
+
+    private static void setupVideoGraph(WeightedGraph<String> graph) {
+        graph.addEdge("A", "B", 2.0);
+        graph.addEdge("A", "C", 3.0);
+        graph.addEdge("A", "D", 3.0);
+
+        graph.addEdge("B", "C", 4.0);
+        graph.addEdge("B", "E", 3.0);
+
+        graph.addEdge("C", "F", 6.0);
+        graph.addEdge("C", "E", 1.0);
+
+        graph.addEdge("D", "F", 7.0);
+
+        graph.addEdge("E", "F", 8.0);
+
+        graph.addEdge("F", "G", 8.0);
     }
 }
--- src/project/WeightedGraph.java
+++ src/project/WeightedGraph.java
@@ -1,13 +1,19 @@
 package project;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
 import java.util.function.Supplier;
 
 public abstract class WeightedGraph<V extends Comparable<V>> {
-    protected static final Comparator<WeightedEdge<?>> WEIGHT_COMPARATOR = Comparator.comparingInt(e -> e.weight);
+    protected static final Comparator<WeightedEdge<?>> WEIGHT_COMPARATOR = Comparator.comparingDouble(e -> e.weight);
 
     private final Map<V, Collection<WeightedEdge<V>>> graph;
     private final Supplier<Collection<WeightedEdge<V>>> collectionSupplier;
@@ -25,35 +31,70 @@ public abstract class WeightedGraph<V extends Comparable<V>> {
         return true;
     }
 
-    public boolean addEdge(V source, V destination, int weight) throws IllegalArgumentException {
+    public boolean addEdge(V source, V destination, double weight) throws IllegalArgumentException {
         addVertex(source);
         addVertex(destination);
 
-        return graph.get(source).add(new WeightedEdge<>(destination, weight)) &&
-                graph.get(destination).add(new WeightedEdge<>(source, weight));
+        graph.get(source).add(new WeightedEdge<>(source, destination, weight));
+        graph.get(destination).add(new WeightedEdge<>(destination, source, weight));
+        return true;
     }
 
-    public int primAlgorithm() {
-        return 0;
+    //TODO - Determine that graph is fully connected
+    public List<WeightedEdge<V>> primsAlgorithm() {
+        if (graph.isEmpty())
+            return null;
+        V node = pickRandomNode();
+        Map<V, WeightedEdge<V>> spanningTree = new HashMap<>(graph.size());
+        PriorityQueue<WeightedEdge<V>> queue = new PriorityQueue<>(WEIGHT_COMPARATOR);
+
+        spanningTree.put(node, null);
+        queue.addAll(graph.get(node));
+
+        while (!queue.isEmpty()) {
+            WeightedEdge<V> currentEdge = queue.poll();
+            if (spanningTree.containsKey(currentEdge.destination))
+                continue;
+
+            spanningTree.put(currentEdge.destination, currentEdge);
+            queue.addAll(graph.get(currentEdge.destination));
+
+            if (spanningTree.size() == graph.size()) break;
+        }
+
+        return new ArrayList<>(spanningTree.values());
     }
 
-    protected record WeightedEdge<V>(V destination, int weight) {
+    private V pickRandomNode() {
+        int randomIndex = (int)(Math.random() * graph.size());
+
+        Iterator<V> iterator = graph.keySet().iterator();
+        V node = iterator.next();
+        while (randomIndex-- != 1) {
+            node = iterator.next();
+        }
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        List<String> strings = graph.values()
+                .stream()
+                .flatMap(Collection::stream)
+                .map(Objects::toString)
+                .toList();
+        return String.join("\n", strings);
+    }
+
+    protected record WeightedEdge<V>(V source, V destination, double weight) {
         public WeightedEdge {
-            if (weight < 0)
+            if (weight < 0.0)
                 throw new IllegalArgumentException("Weight cannot be negative");
         }
 
         @Override
-        public int hashCode() {
-            return destination.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (o == this) return true;
-            if (!(o instanceof WeightedEdge)) return false;
-
-            return this.destination.equals(((WeightedEdge<?>) o).destination);
+        public String toString() {
+            return source + " - " + destination + " Weight: " + weight;
         }
     }
 }
